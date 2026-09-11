f1ace096db17c7ed72a4a45a364397947a4538f8
Sudesh Jha <sudeshjha2005@gmail.com>
2026-09-11 21:31:29 +0530

    add pixel to graph

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
+from datetime import datetime
 
 USERNAME = "sudeshjha"
 TOKEN = "abcdefghijklmnopqrstuvwxyz"
+GRAPH_ID = "graph1"
+HEADERS = {"X-USER-TOKEN": TOKEN}
+
 
 # ------------------------------- CREATE A USER
 PIXELA_USER_ENDPOINT = "https://pixe.la/v1/users"
@@ -18,15 +22,18 @@ user_params = {
 GRAPH_ENDPOINT = f"{PIXELA_USER_ENDPOINT}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": {GRAPH_ID},
     "name": "Running Graph",
     "unit": "km",
     "type": "float",
     "color": "ajisai",
 }
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=HEADERS)
 
-headers = {"X-USER-TOKEN": TOKEN}
+# ------------------------------- CREATE A GRAPH
+PIXEL_CREATION_ENDPOINT = f"{PIXELA_USER_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_data = {"date": "20260911", "quantity": "9.74"}
 
 
-response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+response = requests.post(url=PIXEL_CREATION_ENDPOINT, json=pixel_data, headers=HEADERS)
 print(response.text)
